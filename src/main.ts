1936b92a5091090c7b9a1378f460e0f28525dd1d
javed <javed@local.com>
2023-12-13 12:48:11 +0530

    build: generate data from json and improve style

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+type Marks = {
+  category: string;
+  score: number;
+  icon: string;
+  color: string;
+};
+const OUT_OF = 100;
+
+const updateMarks = (data: Marks[]) => {
+  const marksList = document.querySelector(
+    "[data-marks-list]"
+  ) as HTMLUListElement;
+  const resultAvg = document.querySelector(
+    "[data-result-avg]"
+  ) as HTMLDivElement;
+
+  let totalMarks = 0;
+
+  data.forEach((mark) => {
+    totalMarks += mark.score;
+    const listItem: HTMLLIElement = document.createElement("li");
+    listItem.style.setProperty("--_clr", mark.color);
+    listItem.innerHTML += `<img src=${mark.icon} alt=${mark.category}>`;
+    listItem.innerHTML += `<span>${mark.category}</span>`;
+    listItem.innerHTML += `<span data-marks=""><strong>${mark.score}</strong> / ${OUT_OF}</span>`;
+    marksList.appendChild(listItem);
+  });
+
+  const avg = (totalMarks / (data.length * OUT_OF)) * 100;
+  const roundedAvg = Math.round(avg);
+  resultAvg.innerHTML = `<strong>${roundedAvg}</strong><span>of ${OUT_OF}</span>`;
+};
+
+async function fetchMarks() {
+  try {
+    const res = await fetch("./src/data.json");
+    const data: Marks[] = await res.json();
+    updateMarks(data);
+  } catch (error: any) {
+    console.log(error);
+  }
+}
+
+fetchMarks();
